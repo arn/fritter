f236cab82168a27bb8d25be38465f889f0be98e6
Aaron Norling <aaron@scythia.local>
2008-09-01 10:59:35 -0400

    making rudimentary associations between Message and Category based on flags passed in message.content - this being done as a before save in Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,7 +12,15 @@ class Message < ActiveRecord::Base
   
   # method will find matches to categoris in content and create categorizations
   def categorize
-    puts 'something'
+    logger.info "in method categorize"
+    Category.find(:all).each do |c|
+      logger.info "#{c.name} is the category being tested"
+      if self.content.match(/\-#{c.flag}/) || self.content.match(/\-\-#{c.name}/)
+        self.categorizations.build( :message_id => self.id, :category_id => c.id )
+        logger.info "created categorization"
+      end
+    end
+    logger.info "done testing categories"
   end
   
 end

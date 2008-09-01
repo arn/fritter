23961920d4e6dfb813c69c710e366449717801fe
arn <arn@nrlng.com>
2008-09-01 14:56:55 -0400

    categorize method accounts for flag-like sequences of characters that aren't flags

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,17 +10,12 @@ class Message < ActiveRecord::Base
   @@per_page = 10
   
   
-  # method will find matches to categoris in content and create categorizations
   def categorize
-    logger.info "in method categorize"
     Category.find(:all).each do |c|
-      logger.info "#{c.name} is the category being tested"
-      if self.content.match(/\-#{c.flag}/) || self.content.match(/\-\-#{c.name}/)
+      if self.content.match(/^\-#{c.flag}\b|\s\-#{c.flag}\b/) || self.content.match(/\W\-\-#{c.name}\b/)
         self.categorizations.build( :message_id => self.id, :category_id => c.id )
-        logger.info "created categorization"
       end
     end
-    logger.info "done testing categories"
   end
   
 end
